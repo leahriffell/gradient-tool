ce83c388012ccf8b861781c887f054467bf4db94
Leah Riffell <leah.riffell@gmail.com>
2020-04-20 14:23:20 -0600

    Initial commit

---
+++ gradient.js
@@ -0,0 +1,8 @@
+var hex1 = document.getElementById("hex-1").value;
+var hex2 = document.getElementById("hex-2").value;
+var hex1Display = document.getElementById("hex-1-display");
+var hex2Display = document.getElementById("hex-2-display");
+
+
+hex1Display.textContent = hex1;
+hex2Display.textContent = hex2;
